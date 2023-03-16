5fdecadb91c073dccba0d8355a8b56b78565d661
Devhellom38 <104822733+Devhellom38@users.noreply.github.com>
2023-03-16 10:17:19 +0100

    Update __main__.py

--- src/ethack/__main__.py
+++ src/ethack/__main__.py
@@ -10,10 +10,7 @@ def main() -> None:
     args = [a for a in sys.argv[1:] if not a.startswith("-")]
     opts = [o for o in sys.argv[1:] if o.startswith("-")]
 
-    # Show help message
-    if "-h" in opts or "--help" in opts:
-        viewer.show(__doc__)
-        raise SystemExit()
+
 
   
 
